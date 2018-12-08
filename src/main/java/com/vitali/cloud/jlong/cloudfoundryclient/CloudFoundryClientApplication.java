b3b85cc55b966e12134a514e716b1eca03686009
Vitali Litvinenka <lvitali@setplex.com>
2018-12-08 10:28:59 +0300

    init comit

---
+++ src/main/java/com/vitali/cloud/jlong/cloudfoundryclient/CloudFoundryClientApplication.java
@@ -0,0 +1,13 @@
+package com.vitali.cloud.jlong.cloudfoundryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFoundryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFoundryClientApplication.class, args);
+    }
+
+}
